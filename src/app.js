8d0392d74d88299753ffa27821a66a2ebb2db495
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2022-06-02 12:05:59 -0400

    Zomboid changes

--- src/app.js
+++ src/app.js
@@ -80,7 +80,7 @@ function App() {
     return (
         <div className="flex flex-col items-center space-y-8 pt-8">
             <h1 className="text-5xl text-white tracking-tighter font-mono font-bold select-all">
-                minecraft.aws.hugo.dev.br
+                zomboid.aws.hugo.dev.br
             </h1>
 
             {/* Loading */}
